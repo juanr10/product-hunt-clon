118813b23c6aaee352b82401623755201742d54e
juanr10 <argudojuan7@gmail.com>
2021-06-24 20:29:40 +0200

    Emotion configuration complete & Layout component added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Layout from '../components/layout/Layout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Home</h1>
+    <div>
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
     </div>
   )
 }

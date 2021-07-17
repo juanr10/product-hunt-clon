871ad6dc44c0d7fd9d7cebb043970f292f0e24ca
juanr10 <argudojuan7@gmail.com>
2021-07-17 18:34:17 +0200

    header styles complete

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
 import Link from 'next/link';
+//Styles
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+//Components
 import Search from '../ui/Search';
 import Navigation from './Navigation';
+import Button from '../ui/Button';
+
+// Styled components
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width:768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--orange);
+    font-size: 4rem;
+    line-height: 0;
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
+    const user = true;
+
+
     return ( 
-        <header>
-            <div>
-                <div>
-                    <p>P</p>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gray3);
+                padding: 1rem 0;
+            `}
+        >
+            <HeaderContainer>
+                <div css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    <Link href="/" passHref>
+                        <Logo>P</Logo>
+                    </Link>
+                    
                     {/* Search */}
                     <Search />
 
@@ -16,16 +56,39 @@ const Header = () => {
                     <Navigation />
                 </div>
 
-                <div>
-                    {/* Admin menu */}
-                    <p>Hola: Juan</p>
+                {/* Right elements */}
+                <div css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    { user ? 
+                        (
+                            <>
+                                {/* Admin menu */}
+                                <p css={css`
+                                        margin-right: 2rem;
+                                    `}
+                                >
+                                    Hola: Juan
+                                </p>
 
-                    <button type="button">Log out</button>
-
-                    <Link href="/">Login</Link>
-                    <Link href="/d">Create account</Link>
+                                <Button bgColor="true">Log out</Button>
+                            </>
+                        ) : 
+                        (
+                            <>
+                                <Link href="/" passHref>
+                                    <Button bgColor="true">Login</Button>
+                                </Link>
+                                <Link href="/d" passHref>
+                                    <Button>Create account</Button>
+                                </Link>
+                            </>
+                        )
+                    }
                 </div>
-            </div>
+            </HeaderContainer>
         </header>
      );
 }
--- components/layout/Navigation.jsx
+++ components/layout/Navigation.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+    padding-left: 2rem;
+
+    a {
+        font-size: 1.8rem;
+        margin-left: 2rem;
+        color: var(--gray2);
+        font-family: "PT Sans", serif;
+
+        &:last-of-type {
+            margin-right: 0;
+        }
+    }
+`;
 
 const Navigation = () => {
     return ( 
-        <nav>
+        <Nav>
             <Link href="/">Home</Link>
             <Link href="/">Populars</Link>
             <Link href="/">New Product</Link>
-        </nav>
+        </Nav>
      );
 }
  

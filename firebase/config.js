549f516bd7fb2b33ffce3f9a418e645e732e8f75
juanr10 <argudojuan7@gmail.com>
2021-07-21 13:43:33 +0200

    firebase configuration complete

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYkvDoIVYeNLtj4WYX2-lHiVng0lmzuV0",
+    authDomain: "product-hunt-fa3e0.firebaseapp.com",
+    projectId: "product-hunt-fa3e0",
+    storageBucket: "product-hunt-fa3e0.appspot.com",
+    messagingSenderId: "215694963666",
+    appId: "1:215694963666:web:96d6a26b2e8716598c0e0b"
+};
+
+export default firebaseConfig;
